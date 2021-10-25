bf0a2872f344510070f73e078c804b9d0acfebc8
Push <49253201+RideTheSkyP@users.noreply.github.com>
2021-10-25 13:09:24 +0200

    Update FA.py

--- L1/FA.py
+++ L1/FA.py
@@ -1,24 +1,23 @@
-import sys
-
 # Finite Automata algorithm for Pattern Searching
+import argparse
 
-alphabet = 255
+alphabet = 3000
+positions = list()
 
 
 def getNextState(pattern, patternLength, state, char):
-
     if state < patternLength and char == ord(pattern[state]):
         return state + 1
 
+    index = 0
     for nextState in range(state, 0, -1):
         if ord(pattern[nextState - 1]) == char:
             for i in range(nextState - 1):
                 if pattern[i] != pattern[state - nextState + 1 + i]:
                     break
-                i += 1
-
-                if i == nextState - 1:
-                    return nextState
+                index += 1
+            if index == nextState - 1:
+                return nextState
     return 0
 
 
@@ -40,9 +39,22 @@ def faSearch(text, pattern):
         state = table[state][ord(text[i])]
         if state == len(pattern):
             position = i - len(pattern) + 1
-            print(f"Pattern starts at position: {position}\nHere is visualization of found pattern in text: {text[:position].lower() + text[position:position + len(pattern)] + text[position:].lower()}")
+            positions.append(position)
 
 
-text = sys.argv[2]
-pattern = sys.argv[1]
-faSearch(text, pattern)
+def parseFile(filename):
+    with open(filename, "r") as f:
+        return "".join(f.readlines())
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Finite Automata")
+    parser.add_argument("pattern", help="Pattern we want to find in file.")
+    parser.add_argument("filename", help="Filename.")
+    args = parser.parse_args()
+    faSearch(parseFile(args.filename), args.pattern)
+    print(positions)
+
+
+if __name__ == "__main__":
+    main()
